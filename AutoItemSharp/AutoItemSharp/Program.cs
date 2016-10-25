40b98612e533f03afe742691a7f83b00681d618d
knightfeng <knightfengef@gmail.com>
2016-10-25 11:13:57 -0700

    Does not midas/talon necromonicons.
    	modified:   AutoItemSharp/AutoItemSharp/Program.cs

--- AutoItemSharp/AutoItemSharp/Program.cs
+++ AutoItemSharp/AutoItemSharp/Program.cs
@@ -43,6 +43,7 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
             Game.OnUpdate += Game_OnUpdate;
             Player.OnExecuteOrder += Player_OnExecuteOrder;
+
         }
 
         private static void Player_OnExecuteOrder(Player sender, ExecuteOrderEventArgs args)
@@ -72,14 +73,16 @@ new MenuItem("Enable", "Enable").SetValue(true);
             {
                 Item bottle = me.FindItem("item_bottle");
 
-                
 
-                if (bottle != null && bottle.CanBeCasted() && !me.IsInvisible() && !me.IsChanneling())
+
+                if (bottle != null && bottle.CanBeCasted() && !me.IsInvisible() && !me.IsChanneling() && bottle.Cooldown <= 0)
                 {
                     IEnumerable<Hero> allies = ObjectManager.GetEntities<Hero>().Where(x => x.Team == me.Team && x.IsAlive && (x.Health < x.MaximumHealth || x.Mana < x.MaximumMana) && x.Distance2D(me) <= 600 && x.IsVisible && x.IsSpawned && !x.HasModifier("modifier_bottle_regeneration") && !x.IsIllusion);
-                                                       
+                    IEnumerable<Building> fountains = ObjectManager.GetEntities<Building>().Where(x => x.Name == "dota_fountain");
+                    Building fountain = fountains.FirstOrDefault();
 
-                    if (bottle.Cooldown <= 0 && Utils.SleepCheck("bottle") && me.HasModifier("modifier_fountain_aura_buff"))
+
+                    if (Utils.SleepCheck("bottle") && me.HasModifier("modifier_fountain_aura_buff"))
                     {
                         foreach (Hero ally in allies)
                         {
@@ -91,14 +94,38 @@ new MenuItem("Enable", "Enable").SetValue(true);
                         }
                     }
 
-                    else if (bottle.Cooldown <= 0 && Utils.SleepCheck("bottle") && me.HasModifier("modifier_fountain_aura_buff") && bottle.CurrentCharges == 3 && !me.HasModifier("modifier_bottle_regeneration"))
+
+
+                    else if (Utils.SleepCheck("bottle") && me.HasModifier("modifier_fountain_aura_buff") && me.Distance2D(fountain) >= 1200)
                     {
-                        bottle.UseAbility(me);
-                        Utils.Sleep(250, "bottle");
+                        var start = Game.GameTime;
+
+                        while (Game.GameTime <= start + 3 && Utils.SleepCheck("bottle"))
+                        {
+                            bottle.UseAbility(me);
+                            Utils.Sleep(250, "bottle");
+                            Console.WriteLine(Game.GameTime);
+                        }
+
+
+
                     }
 
                 }
             }
+        }
+
+        private static bool IsNecromonicon(Creep creep)
+        {
+            List<string> necroNames = new List<string>()
+            { "npc_dota_necronomicon_warrior_1",
+                "npc_dota_necronomicon_warrior_2",
+                "npc_dota_necronomicon_warrior_3",
+                "npc_dota_necronomicon_archer_1",
+                "npc_dota_necronomicon_archer_2",
+                "npc_dota_necronomicon_archer_3" };
+
+            return necroNames.Any(x => x.Equals(creep.Name));
         }
 
         private static void AutoTalon()
@@ -107,12 +134,12 @@ new MenuItem("Enable", "Enable").SetValue(true);
             {
                 Item talon = me.FindItem("item_iron_talon");
 
-                if (talon != null)
+                if (talon != null && talon.CanBeCasted())
                 {
-                    IEnumerable<Unit> monsters = ObjectManager.GetEntities<Unit>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(me) <= 600 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned);// && x.UnitType != "Building");
-                    Unit highestHPCreep = monsters.MaxOrDefault(x => x.Health);
+                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(me) <= 600 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned && !IsNecromonicon(x));
+                    Creep highestHPCreep = monsters.MaxOrDefault(x => x.Health);
 
-                    if (talon.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Talon") && highestHPCreep.Distance2D(me) <= 350 && MoreThanAnAttack(highestHPCreep) && highestHPCreep != null )
+                    if (talon.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Talon") && highestHPCreep.Distance2D(me) <= 350 && MoreThanAnAttack(highestHPCreep) && highestHPCreep != null)
                     {
                         talon.UseAbility(highestHPCreep);
                         Utils.Sleep(250, "Talon");
@@ -135,12 +162,12 @@ new MenuItem("Enable", "Enable").SetValue(true);
             {
                 Item midas = me.FindItem("item_hand_of_midas");
 
-                if (midas != null)
+                if (midas != null && midas.CanBeCasted())
                 {
-                    IEnumerable<Unit> monsters = ObjectManager.GetEntities<Unit>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(me) <= 800 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned);
-                    Unit highestHPCreep = monsters.MaxOrDefault(x => x.Health);
+                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(me) <= 800 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned && !IsNecromonicon(x));
+                    Creep highestHPCreep = monsters.MaxOrDefault(x => x.Health);
 
-                    if (midas.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Midas") && highestHPCreep.Distance2D(me) <= 600 && MoreThanAnAttack(highestHPCreep) && highestHPCreep != null )
+                    if (midas.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Midas") && highestHPCreep.Distance2D(me) <= 600 && highestHPCreep != null)
                     {
                         midas.UseAbility(highestHPCreep);
                         Utils.Sleep(250, "Midas");
@@ -155,7 +182,7 @@ new MenuItem("Enable", "Enable").SetValue(true);
             {
                 Item phase = me.FindItem("item_phase_boots");
 
-                if (phase != null && phase.Cooldown <= 0 && Utils.SleepCheck("Phase") && me.IsMoving && !me.IsChanneling() && !me.IsInvisible() )
+                if (phase != null && phase.Cooldown <= 0 && Utils.SleepCheck("Phase") && me.IsMoving && !me.IsChanneling() && !me.IsInvisible())
                 {
                     phase.UseAbility();
                     Utils.Sleep(250, "Phase");
