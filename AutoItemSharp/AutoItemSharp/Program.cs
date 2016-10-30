779b7d1e7d068b3ed15ffe8f82f8b0a1ccfa8819
knightfeng <knightfengef@gmail.com>
2016-10-29 21:39:04 -0700

    Added auto-deward.
    	modified:   AutoItemSharp/AutoItemSharp/Program.cs

--- AutoItemSharp/AutoItemSharp/Program.cs
+++ AutoItemSharp/AutoItemSharp/Program.cs
@@ -33,6 +33,10 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
         private static Hero me;
         private static Player player;
+        private static int cutRange;
+
+
+        private static readonly string[] cutters = { "item_quelling_blade", "item_iron_talon", "item_bfury", "item_tango_single", "item_tango" };
 
         private static void Main(string[] args)
         {
@@ -46,6 +50,7 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
         }
 
+        //BUGGY SINCE IF STOP OR HOLD IS PRESSED PHASE TRIGGERED.
         private static void Player_OnExecuteOrder(Player sender, ExecuteOrderEventArgs args)
         {
             if (me.Distance2D(args.TargetPosition) >= Menu.Item("Phase Distance").GetValue<Slider>().Value)
@@ -55,7 +60,7 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
             if (me.Name.Equals("npc_dota_hero_lone_druid") || me.Name.Equals("npc_dota_hero_rubick"))
             {
-                var bear = ObjectManager.GetEntities<Unit>().Where(x => x.IsControllable && x.Name.Contains("npc_dota_lone_druid_bear") && x.IsAlive && !x.IsIllusion && x.IsSpawned && x.Health > 0).FirstOrDefault();
+                var bear = ObjectManager.GetEntities<Unit>().FirstOrDefault(x => x.IsControllable && x.Name.Contains("npc_dota_lone_druid_bear") && x.IsAlive && !x.IsIllusion && x.IsSpawned && x.Health > 0);
                 if (bear != null)
                 {
                     if (bear.Distance2D(args.TargetPosition) >= Menu.Item("Phase Distance").GetValue<Slider>().Value) AutoPhase(bear);
@@ -68,12 +73,16 @@ new MenuItem("Enable", "Enable").SetValue(true);
             player = ObjectManager.LocalPlayer;
             me = ObjectManager.LocalHero;
 
-            if ((!Menu.Item("Enable").GetValue<bool>()) || !Game.IsInGame || player == null || me == null || Game.IsChatOpen || Game.IsPaused || me.IsChanneling() || me.IsInvisible()) return;
+            if ((!Menu.Item("Enable").GetValue<bool>()) || !Game.IsInGame || player == null || me == null || Game.IsChatOpen || Game.IsWatchingGame || Game.IsPaused || me.IsChanneling() || me.IsInvisible()) return;
 
             AutoMidas(me);
             AutoTalon(me);
             AutoBottle();
-            if (me.Name.Equals("npc_dota_hero_lone_druid") || me.Name.Equals("npc_dota_hero_rubick")) Bear();
+            AutoDeward(me);
+            if (me.Name.Equals("npc_dota_hero_lone_druid") || me.Name.Equals("npc_dota_hero_rubick"))
+            { 
+                    Bear();
+            }
 
         }
 
@@ -137,19 +146,6 @@ new MenuItem("Enable", "Enable").SetValue(true);
             }
         }
 
-        private static bool IsNecromonicon(Creep creep)
-        {
-            List<string> necroNames = new List<string>()
-            { "npc_dota_necronomicon_warrior_1",
-                "npc_dota_necronomicon_warrior_2",
-                "npc_dota_necronomicon_warrior_3",
-                "npc_dota_necronomicon_archer_1",
-                "npc_dota_necronomicon_archer_2",
-                "npc_dota_necronomicon_archer_3" };
-
-            return necroNames.Any(x => x.Equals(creep.Name));
-        }
-
         private static void AutoTalon(Unit unit)
         {
             if (ItemKeyItem.GetValue<AbilityToggler>().IsEnabled("item_iron_talon"))
@@ -158,10 +154,10 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
                 if (talon != null && talon.CanBeCasted())
                 {
-                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(unit) <= 600 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned && !IsNecromonicon(x));
+                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(unit) <= 600 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned && !x.Name.Contains("npc_dota_necronomicon_warrior") && !x.Name.Contains("npc_dota_necronomicon_archer"));
                     Creep highestHPCreep = monsters.MaxOrDefault(x => x.Health);
 
-                    if (talon.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Talon") && highestHPCreep.Distance2D(unit) <= 350 && MoreThanAnAttack(highestHPCreep, unit) && highestHPCreep != null)
+                    if (talon.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Talon") && highestHPCreep.Distance2D(unit) <= 350 && MoreThanAnAttack(highestHPCreep, unit) && highestHPCreep != null && unit.IsAlive)
                     {
                         talon.UseAbility(highestHPCreep);
                         Utils.Sleep(250, "Talon");
@@ -186,10 +182,10 @@ new MenuItem("Enable", "Enable").SetValue(true);
 
                 if (midas != null && midas.CanBeCasted())
                 {
-                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(unit) <= 800 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && x.IsSpawned && !IsNecromonicon(x));
+                    IEnumerable<Creep> monsters = ObjectManager.GetEntities<Creep>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsAncient && x.Distance2D(unit) <= 800 && x.IsVisible && x.Health > 0 && !x.IsMagicImmune() && x.BaseMovementSpeed > 0 && !x.Name.Contains("npc_dota_necronomicon_warrior") && !x.Name.Contains("npc_dota_necronomicon_archer"));
                     Creep highestHPCreep = monsters.MaxOrDefault(x => x.Health);
 
-                    if (midas.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Midas") && highestHPCreep.Distance2D(unit) <= 600 && highestHPCreep != null)
+                    if (midas.Cooldown <= 0 && monsters.Count() > 0 && Utils.SleepCheck("Midas") && highestHPCreep.Distance2D(unit) <= 600 && highestHPCreep != null && unit.IsAlive)
                     {
                         midas.UseAbility(highestHPCreep);
                         Utils.Sleep(250, "Midas");
@@ -204,7 +200,7 @@ new MenuItem("Enable", "Enable").SetValue(true);
             {
                 Item phase = unit.FindItem("item_phase_boots");
 
-                if (phase != null && phase.Cooldown <= 0 && Utils.SleepCheck("Phase") && unit.IsMoving && !unit.IsChanneling() && !unit.IsInvisible())
+                if (phase != null && phase.Cooldown <= 0 && Utils.SleepCheck("Phase") && unit.IsMoving && !unit.IsChanneling() && !unit.IsInvisible() && unit.IsAlive)
                 {
                     phase.UseAbility();
                     Utils.Sleep(250, "Phase");
@@ -217,10 +213,43 @@ new MenuItem("Enable", "Enable").SetValue(true);
         {
             var bear = ObjectManager.GetEntities<Unit>().Where(x => x.IsControllable && x.Name.Contains("npc_dota_lone_druid_bear") && x.IsAlive && !x.IsIllusion && x.IsSpawned && x.Health > 0).FirstOrDefault();
 
-            if ((me.Name.Equals("npc_dota_hero_lone_druid") || me.Name.Equals("npc_dota_hero_rubick")) && bear != null)
-            {                
+            if ((me.Name.Equals("npc_dota_hero_lone_druid") || me.Name.Equals("npc_dota_hero_rubick")) && bear != null && bear.IsAlive)
+            {
                 AutoMidas(bear);
                 AutoTalon(bear);
+                AutoDeward(bear);
+            }
+        }
+
+        private static void AutoDeward(Unit unit)
+        {
+            if (!unit.IsAlive) return;
+            Item aether = unit.FindItem("item_aether_lens");
+
+
+            if (aether == null)
+            {
+                cutRange = 450;
+            }
+            else
+            {
+                cutRange = 650;
+            }
+
+            Unit ward = ObjectManager.GetEntities<Unit>()
+                .FirstOrDefault(
+                    x =>
+                        (x.ClassID == ClassID.CDOTA_NPC_Observer_Ward ||
+                         x.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight || x.ClassID == ClassID.CDOTA_NPC_TechiesMines || x.ClassID == ClassID.CDOTA_NPC_Treant_EyesInTheForest)
+                        && x.Team != me.Team && me.NetworkPosition.Distance2D(x.NetworkPosition) < cutRange &&
+                        x.IsVisible && x.IsAlive);
+            var cutter = cutters.Select(x => unit.FindItem(x)).FirstOrDefault(x => x != null && x.CanBeCasted());
+
+            if(ward != null && !unit.IsChanneling() && Utils.SleepCheck("cut") && cutter != null && !((cutter.Name == "item_tango_single" || cutter.Name == "item_tango") && ward.ClassID == ClassID.CDOTA_NPC_TechiesMines))
+            {
+                Console.WriteLine("Reached");
+                cutter.UseAbility(ward);
+                Utils.Sleep(250, "cut");
             }
         }
 
