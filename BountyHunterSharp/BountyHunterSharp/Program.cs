7f0c95ddb5bbb7da32d09e0d382573e0efbad3b1
knightfeng <knightfengef@gmail.com>
2016-11-16 13:05:13 -0800

    Revert "Removed"
    This reverts commit ddfa9569478136871b9f898bf855ee5e9c620ec5.

---
+++ BountyHunterSharp/BountyHunterSharp/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Extensions;
+using Ensage.Common.Menu;
+using Ensage.Common.Objects;
+using SharpDX;
+
+namespace BountyHunterSharp
+{
+    internal class Program
+    {
+        private static readonly Dictionary<Hero, double> HeroDamageDictionary = new Dictionary<Hero, double>();
+        private static readonly Dictionary<Hero, string> HeroSpellDictionary = new Dictionary<Hero, string>();
+        private static readonly Dictionary<Unit, double> UnitDamageDictionary = new Dictionary<Unit, double>();
+        private static readonly Dictionary<Unit, string> UnitSpellDictionary = new Dictionary<Unit, string>();
+        private static readonly Menu Menu = new Menu("BountyHunterSharp", "BountyHunterSharp", true, "npc_dota_hero_bounty_hunter", true);
+
+
+        private static bool _killError = true;
+        private static bool _killStealEnabled;
+
+        private static Player _player;
+        private static Hero _me;
+
+        private static Unit vhero = new Unit();
+
+        private static void Main()
+        {
+
+            Menu.AddToMainMenu();
+            Menu.AddItem(new MenuItem("PB.Enable", "Enable")).SetValue(true);
+            Menu.AddItem(new MenuItem("Auto-Kill", "Auto-Kill")).SetValue(true).SetTooltip("Auto Kill Steal with Shuriken.");
+
+            Game.OnUpdate += Game_OnUpdate;
+            Drawing.OnDraw += Game_OnDraw;
+
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if ((!Menu.Item("PB.Enable").GetValue<bool>())) return;
+
+
+
+            if (!_killStealEnabled)
+            {
+                if (!Game.IsInGame) return;
+
+                _player = ObjectManager.LocalPlayer;
+                _me = ObjectManager.LocalHero;
+
+                if (_player == null || _me == null || _me.ClassID != ClassID.CDOTA_Unit_Hero_BountyHunter) return;
+
+                _killStealEnabled = true;
+                Console.Write("BH SHARP: Loaded!");
+            }
+            else if (!Game.IsInGame || _player == null || _me == null)
+            {
+                _killStealEnabled = false;
+                Console.Write("BH SHARP: UnLoaded!");
+                return;
+            }
+
+            if (!Utils.SleepCheck("BH ULT") || Game.IsPaused) return;
+            Utils.Sleep(100, "BH ULT");
+
+            if (_me == null) return;
+
+            Kill(_me.Spellbook.Spell1, new double[] { 150, 225, 300, 375 }, 1, 400);
+
+            /*
+            if (_me.Spellbook.Spell1.Level > 0 && _me.Spellbook.Spell1.Cooldown == 0)
+            {
+                Console.WriteLine("Shuriken Ready");
+            }
+
+            else
+            {
+                Console.WriteLine("Cool Down:" + _me.Spellbook.Spell1.Cooldown);
+            }
+            */
+
+
+        }
+
+        private static void Kill(Ability ability, IReadOnlyList<double> damage, uint spellTargetType, uint? range = null, string abilityType = "normal", bool lsblock = true, bool throughSpellImmunity = false, IReadOnlyList<double> adamage = null)
+        {
+            if ((!Menu.Item("Auto-Kill").GetValue<bool>())) return;
+            var spellLevel = (int)ability.Level - 1;
+            if (ability.Level <= 0) return;
+
+            double normalDamage = damage[spellLevel];
+    
+            var spellDamageType = ability.DamageType;
+            var spellRange = range ?? (ability.CastRange + 50);
+            var spellCastPoint = (float)(((_killError ? 0 : ability.GetCastPoint(ability.Level)) + Game.Ping) / 1000);
+
+            var enemies = ObjectManager.GetEntities<Hero>().Where(enemy => enemy.Team == _me.GetEnemyTeam() && !enemy.IsIllusion() && enemy.IsVisible && enemy.IsAlive && enemy.Health > 0 && enemy.HasModifier("modifier_bounty_hunter_track")).ToList();
+            foreach (var enemy in enemies)
+            {
+                double spellDamage = normalDamage;
+
+                var damageDone = (float)spellDamage;
+
+                double damageNeeded;
+
+                if (!HeroDamageDictionary.TryGetValue(enemy, out damageNeeded))
+                {
+                    damageNeeded = enemy.Health - damageDone + spellCastPoint * enemy.HealthRegeneration + MorphMustDie(enemy, spellCastPoint);
+                    HeroDamageDictionary.Add(enemy, damageNeeded);
+                    HeroSpellDictionary.Add(enemy, ability.Name);
+                }
+                else
+                {
+                    HeroDamageDictionary.Remove(enemy);
+                    HeroSpellDictionary.Remove(enemy);
+
+                    damageNeeded = enemy.Health - damageDone + spellCastPoint * enemy.HealthRegeneration + MorphMustDie(enemy, spellCastPoint);
+
+                    HeroDamageDictionary.Add(enemy, damageNeeded);
+                    HeroSpellDictionary.Add(enemy, ability.Name);
+                }
+                if (_me.IsChanneling()) return;
+
+                if (!(damageNeeded < 0) || !(_me.Distance2D(enemy) < spellRange || abilityType.Equals("global")) || !MeCanSurvive(enemy, _me, ability, damageDone)) continue;
+
+                switch (spellTargetType)
+                {
+                    case 1:
+                        CastSpell(ability, enemy, _me, lsblock);
+                        break;
+                }
+                break;
+            }
+        }
+
+        private static float MorphMustDie(Hero target, float value)
+        {
+            if (target.ClassID != ClassID.CDOTA_Unit_Hero_Morphling) return 0;
+
+            var morphLevel = target.Spellbook.Spell3.Level;
+            if (morphLevel <= 0) return 0;
+
+            uint[] morphGain = { 38, 76, 114, 190 };
+            if (target.Modifiers.FirstOrDefault(modifier => modifier.Name == "modifier_morphling_morph_agi") != null && target.Strength > 1)
+                return value * (0 - morphGain[morphLevel - 1] + 1);
+            if (target.Modifiers.FirstOrDefault(modifier => modifier.Name == "modifier_morphling_morph_str") != null && target.Agility > 1)
+                return value * morphGain[morphLevel - 1];
+            return 0;
+        }
+
+
+
+
+        private static void Game_OnDraw(EventArgs args)
+        {
+            if (!Game.IsInGame || _player == null || _me == null)
+                return;
+
+            if ((!Menu.Item("PB.Enable").GetValue<bool>())) return;
+
+            var enemies = ObjectManager.GetEntities<Hero>().Where(hero => hero.IsVisible && hero.IsAlive && !hero.IsIllusion()).ToList();
+            foreach (var enemy in enemies)
+            {
+                if ((!Menu.Item("Auto-Kill").GetValue<bool>())) continue;
+                
+                Vector2 screenPos;
+                var enemyPos = enemy.Position + new Vector3(0, 0, enemy.HealthBarOffset);
+                if (!Drawing.WorldToScreen(enemyPos, out screenPos)) continue;
+
+                var start = screenPos + new Vector2(-51, -40);
+                double damageNeeded;
+                string spell;
+                if (!HeroDamageDictionary.TryGetValue(enemy, out damageNeeded) || !HeroSpellDictionary.TryGetValue(enemy, out spell)) continue;
+
+                var text = "KS:  " + string.Format("{0}", (int)damageNeeded);
+                var textSize = Drawing.MeasureText(text, "Arial", new Vector2(10, 150), FontFlags.None);
+                var textPos = start + new Vector2(51 - textSize.X / 1, -textSize.Y / 1 + 2);
+                //Drawing.DrawRect(textPos - new Vector2(15, 0), new Vector2(10, 10), Drawing.GetTexture("materials/NyanUI/spellicons/" + spell + ".vmt"));
+                Drawing.DrawText(text, "Arial", textPos, new Vector2(20, 0), damageNeeded < 0 ? Color.Red : Color.White, FontFlags.AntiAlias | FontFlags.DropShadow);
+            }            
+        }
+    }
+}
+
+
---
+++ CreepAggro/CreepAggro/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Extensions;
+using Ensage.Common.Menu;
+
+using SharpDX;
+
+namespace CreepAggro
+{
+    class Program
+    {
+        private static readonly Menu Menu = new Menu("CreepAggro", "CreepAggro", true);
+
+        private static readonly MenuItem AggroKeyItem =
+            new MenuItem("aggroKey", "Aggro Key").SetValue(new KeyBind('N', KeyBindType.Press));
+        private static readonly MenuItem UnaggroKeyItem =
+            new MenuItem("unaggroKey", "Unaggro Key").SetValue(new KeyBind('B', KeyBindType.Press));
+
+        private static readonly MenuItem AggroRangeEffectItem =
+            new MenuItem("aggroRangeEffect", "Mark aggroable creeps").SetValue(true);
+
+        private static readonly MenuItem AttackCooldown =
+            new MenuItem("AttackCooldown", "AttackCooldown").SetValue(new Slider(1000, 0, 2000));
+
+        private static readonly MenuItem AggroRange =
+            new MenuItem("AggroRange", "Aggro Range").SetValue(true);
+
+        
+
+        private static readonly Dictionary<Unit, ParticleEffect> Effects = new Dictionary<Unit, ParticleEffect>();
+
+        private static Vector3 _currentMovePosition = new Vector3(0, 0, 0);
+
+        private static bool _isAttacking = false;
+        private static Vector3 _facingDirection;
+
+        public static void Main(string[] args)
+        {
+            AggroRangeEffectItem.ValueChanged += Item_ValueChanged;
+            //AggroRange.ValueChanged += Item_ValueChanged; 
+
+            Menu.AddItem(AggroRangeEffectItem);
+            Menu.AddItem(AggroRange);
+            Menu.AddItem(AggroKeyItem);
+            Menu.AddItem(UnaggroKeyItem);
+            Menu.AddItem(AttackCooldown);
+            Menu.AddToMainMenu();
+
+            Game.OnIngameUpdate += Game_OnUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            var player = ObjectManager.LocalPlayer;
+            if (!Game.IsInGame || player == null || player.Team == Team.Observer) return;
+            
+            Hero me = ObjectManager.LocalHero;
+            var creeps = ObjectManager.GetEntities<Creep>().ToList();
+
+            //RangeEffect(me);
+
+            if (Utils.SleepCheck("aggroDrawing"))
+            {
+                // apply range effect
+                foreach (var creep in creeps)
+                {
+                    HandleEffect(creep, me);
+                }
+
+                Utils.Sleep(Game.Ping + 200, "aggroDrawing");
+            }
+
+
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private static void Item_ValueChanged(object sender, OnValueChangeEventArgs e)
+        {
+            foreach (var particleEffect in Effects.Values)
+            {
+                particleEffect.Dispose();
+            }
+            Effects.Clear();
+        }
+
+        private static void Game_OnUpdate(System.EventArgs args)
+        {
+            var player = ObjectManager.LocalPlayer;
+            if (!Game.IsInGame || player == null || player.Team == Team.Observer || Game.IsChatOpen)
+            {
+                return;
+            }
+
+            var me = ObjectManager.LocalHero;
+            if (me == null || !me.IsAlive)
+            {
+                return;
+            }
+
+            // instantly cancel attack
+            if (_isAttacking)
+            {
+                _isAttacking = false;
+                me.Move(_facingDirection);
+                me.Hold(true);
+            }
+
+            var creeps = ObjectManager.GetEntities<Creep>().ToList();
+
+            if (Utils.SleepCheck("attackSleep"))
+            {
+                Unit target = null;
+
+                // aggro                
+                if (Game.IsKeyDown((AggroKeyItem.GetValue<KeyBind>().Key))) 
+                {
+                    target = GetHeroes(me).FirstOrDefault(x => x.Team != me.Team);
+                }
+
+                // unaggro
+                if (Game.IsKeyDown((UnaggroKeyItem.GetValue<KeyBind>().Key)))
+                {
+                    target = GetHeroes(me).FirstOrDefault(x => x.Team == me.Team);
+                }
+
+                if (target != null)
+                {
+                    me.Attack(target);
+                    _facingDirection = Prediction.InFront(me, 25);
+                    _isAttacking = true;
+                    Utils.Sleep(Game.Ping + AttackCooldown.GetValue<Slider>().Value, "attackSleep");
+                }
+            }
+        }
+
+        private static bool IsAggroable(Unit x, Hero me)
+        {
+            return x != null && x.IsValid && x.IsAlive && x.Team != me.Team
+                && me.Distance2D(x) <= 500 && me.IsVisibleToEnemies;
+        }
+
+        private static List<Hero> GetHeroes(Hero me)
+        {
+            return ObjectManager.GetEntities<Hero>()
+                .Where(x => x != null && x.IsValid && x.IsAlive && x.IsVisible)
+                .OrderBy(me.Distance2D).ToList();
+        }
+
+        static void HandleEffect(Unit unit, Hero me)
+        {
+            if (IsAggroable(unit, me) && GetHeroes(me).Any())
+            {
+                ParticleEffect effect;
+                if (!Effects.TryGetValue(unit, out effect) && AggroRangeEffectItem.GetValue<bool>())
+                {
+                    effect = unit.AddParticleEffect(@"particles\units\heroes\hero_beastmaster\beastmaster_wildaxe_glow.vpcf");
+                    Effects.Add(unit, effect);
+                }
+            }
+            else
+            {
+                ParticleEffect effect;
+                if (Effects.TryGetValue(unit, out effect))
+                {
+                    effect.Dispose();
+                    Effects.Remove(unit);
+                }
+            }
+        }
+
+        /*
+        static void RangeEffect (Hero me)
+        {
+            if (AggroRange.GetValue<bool>())
+            {
+                ParticleEffect range;
+                int aggroRangeDist = 500;
+                range = me.AddParticleEffect(@"particles\ui_mouseactions\drag_selected_ring.vpcf");
+                range.SetControlPoint(2, new Vector3(aggroRangeDist, 255, 0));
+                range.SetControlPoint(1, new Vector3(0, 255, 0));
+            }            
+        }
+        */
+
+
+
+    }
+}
